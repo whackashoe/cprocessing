4e79726130ac235fb34f2f6391efc8b9a086aaba
Jett LaRue <whackashoe@gmail.com>
2012-06-11 06:44:18 -0500

    add windows only magick initializer

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ using namespace cprocessing;
 using namespace Magick;
 
 int main (int argc,char **argv) {
-    Magick::InitializeMagick(*argv);
+	#if defined(__WIN32__) || defined(_WIN32) || defined(WIN32) || defined(__WINDOWS__) || defined(__TOS_WIN__)
+		Magick::InitializeMagick(*argv);	//initialization not required in systems that can support installing to known locations
+    #endif 	
 	run();
 }
---
+++ style.hpp
@@ -0,0 +1,38 @@
+/*
+
+#include "cprocessing.hpp"
+
+using namespace cprocessing;
+
+namespace cprocessing {
+    struct Style {
+        int rectMode;
+        int ellipseMode;
+        int imageMode;
+        int colorMode;
+
+        int ellipseDetail;
+        int sphereDetail;
+
+        int strokeWeight;
+
+        color stroke;
+        color fill;
+
+        Style ( ) {
+            if(styleList.size() > 1) {
+                rectMode;
+                ellipseMode;
+                imageMode;
+                colorMode;
+                ellipseDetail;
+                sphereDetail;
+                strokeWeight;
+                stroke;
+                fill;
+            }
+        }
+    };
+}
+
+*/
