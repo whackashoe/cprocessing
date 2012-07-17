a00fab4de5ffd8bc9d176bf78011f72633751cfe
Jett LaRue <whackashoe@gmail.com>
2012-07-17 01:01:43 -0500

    removed some old files

--- mousepressed.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * mousepressed.cpp
- *
- *  Created on: Apr 29, 2011
- *      Author: esperanc
- */
-
-void mousePressed () {
-}
--- mousereleased.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * mousereleased.cpp
- *
- *  Created on: Apr 29, 2011
- *      Author: esperanc
- */
-void mouseReleased () {
-
-}
