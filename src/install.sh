5edecbde72619a556e3b0fcd720bf6dce107e37a
jett <pizzapi@ubuntu.(none)>
2013-01-16 00:11:13 -0600

    add check for include dir

--- src/install.sh
+++ src/install.sh
@@ -8,6 +8,10 @@ incDest="/usr/local/include"
 
 make -j6 || { echo 'make failed' ; exit 1; }
 
+if [ ! -d "$incDest/cprocessing" ]; then
+	sudo mkdir /usr/local/include/cprocessing || { echo 'creating /usr/local/include/cprocessing failed'; exit 2; }
+fi
+
 sudo cp *hpp $incDest/cprocessing || { echo 'copying headers to $incDest/cprocessing failed'; exit 2; }
 
 cd objects || { echo 'cd objects failed, run mkdir objects'; exit 3; }
